658f6c1558645c01fe7cdd6a86ace1374bee27b0
developer <developer@email.com>
2021-02-01 21:32:13 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+import './assets/sass/style.scss';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import 'jquery/dist/jquery.slim';
+
+import 'popper.js/dist/popper';
+
+import '@fortawesome/fontawesome-free/js/all';
+
+// import 'video.js/dist/video.min.js';
+
+// import 'video.js/dist/video-js.min.css';
+
+//  add normalize css

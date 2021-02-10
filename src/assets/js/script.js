cd4dd6a205676c5446c6cc9a0594417a444237a9
developer <developer@email.com>
2021-02-10 13:49:50 +0100

    input Slider Range : Price

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -21,19 +21,34 @@ jQuery(function () {
         $('html,body').animate({scrollTop: $(hash).offset().top},800,function(){})
     });
 
-    // Slider Range
+    // Slider Range Experience Years min : 2 years - max : 50 years
 
-    $( "#slider-range" ).slider({
+        $( "#slider-range" ).slider({
         range: true,
         min: 2,
         max: 50,
-        values: [ 2, 50 ],
+        values: [ 5, 20 ],
         slide: function( event, ui ) {
-          $( "#experience" ).val(ui.values[ 0 ] +  " Years "  +  "  -  " + ui.values[ 1 ] + " Years " );
+            $( "#experience" ).val(ui.values[ 0 ] +  " Years "  +  "  -  " + ui.values[ 1 ] + " Years " );
         }
-      });
-      $( "#experience" ).val( $( "#slider-range" ).slider( "values", 0 ) + " Years" +
+        });
+        $( "#experience" ).val( $( "#slider-range" ).slider( "values", 0 ) + " Years" +
         "  -  " + $( "#slider-range" ).slider( "values", 1 ) + " Years " );
+
+
+    // Slider Range Price min : 50 Dollar - max : 1000 Dollar
+
+    $( "#slider-range-one" ).slider({
+    range: true,
+    min: 50,
+    max: 1000,
+    values: [ 50, 1000 ],
+    slide: function( event, ui ) {
+        $( "#Price" ).val(ui.values[ 0 ] +  " Dollar "  +  "  -  " + ui.values[ 1 ] + " Dollar " );
+    }
+    });
+    $( "#Price" ).val( $( "#slider-range-one" ).slider( "values", 0 ) + " Dollar" +
+    "  -  " + $( "#slider-range-one" ).slider( "values", 1 ) + " Dollar " );
 });
 
 
